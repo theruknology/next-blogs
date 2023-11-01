586f44a36b6b358205d7ac9682a917a880871b7a
Mohammed Ruknuddin <iosrukn123@gmail.com>
2023-11-01 23:28:52 +0300

    pages and routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
+      <body className={`font-sans ${inter.variable}`}>
+        <nav>My Navbar</nav>
+        {children}
+      </body>
     </html>
   );
 }
